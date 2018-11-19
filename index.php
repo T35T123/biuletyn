3d712566f5fa59677ecfbffeeb7fbb07b1d172d0
wojtek2kdev <wojtek2kdev@gmail.com>
2018-11-19 12:41:40 +0100

    Update index.php and add style.css 	- add axios lib 	- add google font 	- make basic layout

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+  <script src="weather.js"></script>
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">   
+  <title></title>
+</head>
+<body>
+  <div id="app">
+    <div id="weather"></div>
+    <div id="number"></div>
+    <div id="cinema">
+      <h1>Wyjscie do kina</h1>
+    </div>
+    <main>
+      <div id="newses"></div>
+    </main> 
+    <div id="contests"></div>
+  </div>
+</body>
+</html>
+
